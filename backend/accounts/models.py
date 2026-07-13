37acec0d29ac9c9c8c6117a88eb5792cc058470d
= <=>
2026-07-14 01:21:37 +0600

    Created custom user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .managers import CustomUserManager
+
 class Tutor(AbstractUser):
-    
+    username = None  # Remove the username field
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    objects = CustomUserManager()
+    def __str__(self):
+        return self.name
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accounts.app.AccountsCofig",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+AUTH_USER_MODEL = "accounts.Tutor"
